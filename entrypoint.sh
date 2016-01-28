150a3149e4934a654527787cd87a0d1d006ae401
Pavitra Bhalla <pavitrabhalla@gmail.com>
2016-01-28 14:55:06 -0800

    No need to build the application

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,11 +8,9 @@ set -e
 : ${APP_DIR:="/var/www"}
 : ${SRC_DIR:="/src/app"}
 : ${BRANCH:="master"}
-: ${MONGO_URL:="mongodb://${MONGO_PORT_27017_TCP_ADDR}:${MONGO_PORT_27017_TCP_PORT}/${DB}"}
 : ${PORT:="80"}
 : ${RELEASE:="latest"}
 
-#export MONGO_URL
 export PORT
 
 # If we were given arguments, run them instead
@@ -41,7 +39,6 @@ ENDHERE
 fi
 
 # Make sure critical directories exist
-mkdir -p $APP_DIR
 mkdir -p $SRC_DIR
 
 node -v
@@ -91,54 +88,17 @@ if [ -n "${METEOR_DIR}" ]; then
    /tmp/meteor.sh
    rm /tmp/meteor.sh
 
-   # Bundle the Meteor app
-   echo "Building the bundle...(this may take a while)"
-   mkdir -p ${APP_DIR}
-   set +e # Allow the next command to fail
-   meteor build --directory ${APP_DIR}
-   if [ $? -ne 0 ]; then
-      echo "Building the bundle (old version)..."
-      set -e
-      # Old versions used 'bundle' and didn't support the --directory option
-      meteor bundle bundle.tar.gz
-      tar xf bundle.tar.gz -C ${APP_DIR}
-   fi
-   set -e
-fi
-
-# If we were given a BUNDLE_URL, download the bundle
-# from there.
-if [ -n "${BUNDLE_URL}" ]; then
-   echo "Downloading Application bundle from ${BUNDLE_URL}..."
-   curl ${CURL_OPTS} -o /tmp/bundle.tgz ${BUNDLE_URL}
-   tar xf /tmp/bundle.tgz -C ${APP_DIR}
-fi
-
-# Locate the actual bundle directory
-# subdirectory (default)
-BUNDLE_DIR=$(find ${APP_DIR} -type d -name bundle -print |head -n1)
-if [ ! -n "${BUNDLE_DIR}" ]; then
-   # No bundle inside app_dir; let's hope app_dir _is_ bundle_dir...
-   BUNDLE_DIR=${APP_DIR}
-fi
-
 # Install NPM modules
-if [ -e ${BUNDLE_DIR}/programs/server ]; then
+if [ -e ${METEOR_DIR} ]; then
    echo "Installing NPM prerequisites..."
-   pushd ${BUNDLE_DIR}/programs/server/
-   npm install npm -g
+   pushd ${METEOR_DIR}
    npm install
    popd
 else
    echo "Unable to locate server directory in ${BUNDLE_DIR}; hold on: we're likely to fail"
 fi
 
-if [ ! -e ${BUNDLE_DIR}/main.js ]; then
-   echo "Failed to locate main.js in ${BUNDLE_DIR}; cannot start application."
-   exit 1
-fi
-
 # Run meteor
-cd ${BUNDLE_DIR}
+cd ${METEOR_DIR}
 echo "Starting Meteor Application..."
 exec meteor
